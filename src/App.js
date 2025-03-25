7bfb7668071cbc8b77cb9f640b0f2560eae58000
MarketingSimonMaree <74555125+MarketingSimonMaree@users.noreply.github.com>
2025-03-25 13:52:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const lastTimeRef = useRef(0);
 
   // Video loop patroon configuratie
-  const audioLoops = 1;    // Aantal loops met geluid aan
+  const audioLoops = 0;    // Aantal loops met geluid aan
   const muteLoops = 3;     // Aantal loops met geluid uit
   const totalPattern = audioLoops + muteLoops;
 
